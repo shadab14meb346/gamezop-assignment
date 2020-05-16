f57682fd91a6c969deda3d12fb63ff9b40249411
Shadab <shadab14meb346@gmail.com>
2020-05-16 22:10:04 +0530

    add drawer tabs

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+import "../styles/drawer.css";
+
+const closeNav = (setOpenSideNav: Function) => {
+	console.log("CLOSE");
+	setOpenSideNav(false);
+};
+
+const openNav = (setOpenSideNav: Function) => {
+	console.log("OPEN");
+	setOpenSideNav(true);
+};
+const Drawer = () => {
+	const [openSideNav, setOpenSideNav] = useState(false);
+	return (
+		<div className='inline'>
+			<div className={`sidenav ${openSideNav ? "open" : "close"}`}>
+				<span className='closebtn' onClick={() => closeNav(setOpenSideNav)}>
+					&times;
+				</span>
+				<a href='#'>Home</a>
+				<a href='#'>Sports</a>
+				<a href='#'>News</a>
+				<a href='#'>Log out</a>
+			</div>
+			<span
+				className={`open-button ${openSideNav ? "close" : "open"}`}
+				onClick={() => openNav(setOpenSideNav)}>
+				&#9776;
+			</span>
+		</div>
+	);
+};
+
+export default Drawer;
