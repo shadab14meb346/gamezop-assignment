6bc5510382967c0263fac4ea42657086e940d1bc
Shadab <shadab14meb346@gmail.com>
2020-05-16 22:23:42 +0530

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,98 @@
+export {};
+import React, { useState, useEffect } from "react";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect,
+} from "react-router-dom";
+import "../styles/header.css";
+import UsersTable from "./UsersTable";
+import Search from "./Search";
+import SearchResults from "./SearchResults";
+import Drawer from "./Drawer";
+const URL = "https://jsonplaceholder.typicode.com/users";
+let loading = false;
+
+const Header = () => {
+	const [users, setUsers] = useState([]);
+	const [blockedUsers, setBlockedUsers] = useState([]);
+	const [searchInput, setSearchInput] = useState("");
+	const [selectedUser, setSelectedUser] = useState({});
+	const [active, setActive] = useState("users");
+	const [topUsers, setTopUsers] = useState([]);
+	const handleLinkClick = (link: string) => {
+		setSelectedUser({});
+		setActive(link);
+	};
+	useEffect(() => {
+		loading = true;
+		fetch(URL)
+			.then((response) => response.json())
+			.then((users) => {
+				console.log(users);
+				loading = false;
+				setUsers(users);
+			})
+			.catch((err) => {
+				loading = false;
+				console.log(err);
+			});
+	}, []);
+	return (
+		<Router>
+			<div>
+				<div className='navigation'>
+					<nav className='navbar'>
+						<Drawer />
+						<Redirect exact from='/' to='users' />
+						<Link
+							onClick={() => {
+								handleLinkClick("users");
+							}}
+							className={`link ${active === "users" ? "active" : null}`}
+							to='/users'>
+							Users
+						</Link>
+						<Link
+							onClick={() => {
+								handleLinkClick("top-users");
+							}}
+							className={`link ${active === "top-users" ? "active" : null}`}
+							to='/top-users'>
+							Top Users
+						</Link>
+						<Search
+							users={users}
+							searchInput={searchInput}
+							setSearchInput={setSearchInput}
+							setSelectedUser={setSelectedUser}
+						/>
+						<SearchResults selectedUser={selectedUser} />
+						<Switch>
+							<Route path='/users'>
+								<UsersTable
+									users={users}
+									setTopUsers={setTopUsers}
+									setBlockedUsers={setBlockedUsers}
+									routeType={"users"}
+								/>
+							</Route>
+							<Route path='/top-users'>
+								<UsersTable
+									users={topUsers}
+									setTopUsers={setTopUsers}
+									setBlockedUsers={setBlockedUsers}
+									routeType={"top-users"}
+								/>
+							</Route>
+						</Switch>
+					</nav>
+				</div>
+			</div>
+		</Router>
+	);
+};
+
+export default Header;
